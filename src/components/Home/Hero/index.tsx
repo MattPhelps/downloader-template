98b7d89f74b04fdb404c9ba7a70e57de4fbb9a9e
Matt Phelps <matt@leandme.com>
2023-12-29 10:49:05 +0100

    ok

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -5,34 +5,115 @@ import siteConfig from "../../../../siteConfig";
 import Render from "../../../components/Render/render";
 import DownloadFail from "../../../components/DownloadFail/downloadFail";
 import { useState, useRef } from "react";
-
+require('dotenv').config();
+import {TailSpin} from "react-loader-spinner"; 
 const Hero = () => {
-
-  const [showRender, setShowRender] = useState(false);
+  const [videoUrl, setVideoUrl] = useState(''); 
+  const [showRender, setShowRender] = useState(false); 
+  const [renderInfo, setRenderInfo] = useState({}); 
   const renderRef = useRef(null); // Create a ref for the Render component
+  const [loading, setLoading] = useState(false);
 
+  const handleDownloadClick = async (event) => {
+    try {
+      event.preventDefault();
 
-  const handleDownloadClick = (event) => {
-    
-    if (siteConfig.functional == "no") {
-      window.open(siteConfig.smartlink, '_blank');
+      // Get the input value
+      if (!videoUrl) {
+        console.error('Please enter a valid video URL.');
+        return;
+      }
+      setLoading(true);
+      
+      const apiUrl =
+        `https://api.apify.com/v2/acts/alexey~youtube-and-all-videos/run-sync-get-dataset-items?token=${siteConfig.apify}`;
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          startUrls: [
+            {
+              url: videoUrl,
+            },
+          ],
+          maxRequestRetries: 5,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data. Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data,"-----------------------------------------------")
+      const videoInfo = data && data[0] ? data[0] : null;
+
+      if (videoInfo) {
+        console.log('Video Information:', videoInfo);
+
+        // Pass the relevant information to the Render component
+        setShowRender(true);
+        setLoading(false);
+        setRenderInfo(videoInfo);
+
+        // Scroll to the Render component after state update
+        setTimeout(() => {
+          renderRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }, 0);
+      } else {
+        console.error('No video information found.');
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
     }
-
-   event.preventDefault(); // Prevents the default link behavior - ADD or remove the href on the button once DONE UVA STUFF
-    setShowRender(true); // Sets the showRender state to true
-    // Scroll to the Render component after state update
-    setTimeout(() => {
-      renderRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }, 0);
   };
+
 
   return (
     <section
       id="home"
       className="relative overflow-hidden z-10 pt-10 md:pt-25 xl:pt-30"
     >
+      {/* <!-- Hero Bg Shapes --> */}
+      <div className="max-w-7xl mx-auto">
+        <div className="absolute -z-10 pointer-events-none inset-0 overflow-hidden -mx-28">
+          <div className="absolute -z-1 -top-[128%] sm:-top-[107%] xl:-top-[73%] left-1/2 -translate-x-1/2 -u-z-10 dark:hero-circle-gradient w-full h-[1282px] rounded-full max-w-[1282px]"></div>
+          <div className="absolute -z-1 -top-[112%] sm:-top-[93%] xl:-top-[62%] left-1/2 -translate-x-1/2 -u-z-10 dark:hero-circle-gradient w-full h-[1046px] rounded-full max-w-[1046px]"></div>
+          <div className="absolute dark:flex hidden top-0 left-1/2 -translate-x-1/2 -u-z-10 max-w-[1204px] w-full aspect-[1204/394]">
+            <Image
+              src="/images/blur/blur-02.svg"
+              alt="blur"
+              fill
+              className="max-w-none"
+            />
+          </div>
+          <div className="absolute dark:flex hidden w-full h-full bg-[url(/images/blur/blur-01.svg)] bg-no-repeat bg-cover bg-top top-0 left-1/2 -translate-x-1/2 -u-z-10"></div>
+        </div>
+      </div>
 
       {/* <!-- Hero Content --> */}
+      {loading ? (
+        <div className="overlay" style={{ 
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backdropFilter: 'blur(8px)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 1000,
+          }}>
+            <div className="centered-spinner" style={{ zIndex: 1001 }}>
+              <TailSpin visible={loading} width={50} height={50} color={siteConfig.buttonColor}/>
+            </div>
+          </div>
+
+          ) : (
+            ''
+          )}
       <div className="mx-auto max-w-[900px] px-4 sm:px-8 xl:px-0 relative z-1">
         <div className="text-center">
           <h1 className="text-[black] dark:text-white mb-6 text-3xl font-extrabold sm:text-5xl xl:text-heading">
@@ -45,12 +126,14 @@ const Hero = () => {
           <div className="flex justify-center items-center mb-4">
             <input
               type="text"
+              value={videoUrl}
+              onChange={(e) => setVideoUrl(e.target.value)}
               placeholder="Paste your video link..."
               className="w-full md:w-96 p-3 rounded-l-lg rounded-r-lg border-2 border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 mr-2"
             />
 
             <Link
-              href="/itubego"
+              href={siteConfig.promotedBrandURL}
               onClick={handleDownloadClick}
               target="_blank"
               rel="noopener noreferrer"
@@ -59,11 +142,11 @@ const Hero = () => {
             >
               Download
             </Link>
-          </div>
+            
+        </div>
         </div>
       </div>
       <div className="flex justify-center items-center w-full"></div>
-
       <div
          className="aspect-[1170/411] bg-red-500 relative max-w-[1170px] w-full mx-auto" // Added margin-top (mt-4)
          data-wow-delay="0.1s"
@@ -74,7 +157,7 @@ const Hero = () => {
           ref={renderRef} // Attach the ref to this div
           className="flex justify-center items-center w-full h-screen" // Added flexbox for centering
         >
-          <Render />
+          <Render  renderInfo={renderInfo}/>
         </div>
       )}
       </div>
--- src/components/Render/render.tsx
+++ src/components/Render/render.tsx
@@ -1,115 +1,196 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React, { useState } from "react";
 import siteConfig from "../../../siteConfig";
+import { TailSpin } from "react-loader-spinner";
 
-const Render = () => {
-    const color = siteConfig.buttonColor
+const Render = ({ renderInfo }) => {
+  const [selectedResolutionIndex, setSelectedResolutionIndex] = useState(0);
+  const [filteredFormats, setFilteredFormats] = useState(
+    renderInfo.formats.filter((r) => !r.url.includes('manifest')).reverse().splice(1,10)
+  );
+  const isResolutionSelected = selectedResolutionIndex !== null;
+  const handleResolutionChange = (index) => {
+    setSelectedResolutionIndex(index);
+  };
+  const color = siteConfig.buttonColor;
+  const [loading, setLoading] = useState(false);
+  const handleDownloadButtonClick = async (url, fileName) => {
+    try {
+      setLoading(true);
+      const corsProxyUrl = "";
+      const proxyUrl = corsProxyUrl + url;
+      const response = await fetch(proxyUrl);
 
-    const handleButtonClick = () => {
-        if (siteConfig.functional == "yes") {
-            window.open(siteConfig.smartlink, '_blank');
-          }
-       
-    };
+      if (!response.ok) {
+        throw new Error(`Failed to fetch video file. Status: ${response.status}`);
+      }
 
-    let promotedURL;
+      const blob = await response.blob();
 
-    if (siteConfig.promotedBrand == "iTubeGo") {
-        promotedURL = "itubego"
+      // Check if the file is WebP or WebM and change the extension to ".mp4"
+      console.log(fileName)
+      const hasFileExtension = /\.\w+$/g.test(fileName);
+      let modifiedFileName = hasFileExtension ? fileName : `${fileName}.mp4`;
+  
+      // Check if the file is WebP or WebM and change the extension to ".mp4"
+      if (url.toLowerCase().endsWith(".webp") || url.toLowerCase().endsWith(".webm")) {
+        modifiedFileName = `${modifiedFileName}.mp4`;
+      }
+console.log(modifiedFileName)
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = modifiedFileName;
+
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      URL.revokeObjectURL(link.href);
+    } catch (error) {
+      console.error("Error downloading video:", error.message);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    if (siteConfig.promotedBrand == "YTSaver") {
-        promotedURL = "ytsaver"
+
+  const handleButtonClick = () => {
+    if (siteConfig.functional == "yes") {
+      window.open(siteConfig.smartlink, "_blank");
     }
+  };
 
-    return (
-        <div className='flex items-center gap-[10px] flex-col justify-center mt-[10px] w-full'>
-            <p className='text-[18px] text-center leading-[1] max-w-[380px] lg:max-w-[540px] mb-4'>
-                Video Title 
-            </p>
-            
-            <div className='flex flex-col lg:flex-row gap-[0px] lg:gap-[10px] items-center lg:items-center rounded-[10px] px-[10px]' >
-                <div className='px-[10px] sm:px-0 mr-[20px] lg:mr-[30px]'>
-                    <Image style={{ width: "auto", height: "auto" }}
-                    className='max-h-[210px] lg:max-h-[190px] rounded-[10px]'
-                    src="/images/render/thumbnail.jpg"
-                    alt="" width={1200} height={600} />
-                </div>
-                <div className='flex flex-col gap-[20px] justify-center lg:px-0 px-[40px] sm:px-0 sm:max-w-[300px] h-full lg:max-w-[210px] py-[15px] items-center'>
-    <div className='flex flex-col gap-[5px] lg:gap-[20px] w-full h-full'>
-        <div className='w-full flex flex-col gap-[5px]'>
-            <button
-                onClick={handleButtonClick} 
-                style={{ "--main-color": siteConfig.buttonColor } as any}
-                className="
-                button-styling
-                justify-center
-                inline-flex
-                rounded-lg
-                py-3
-                px-5
-                text-white
-                font-medium
-                ease-in
-                duration-300
-                hover:opacity-80"
-            >
-                Download
-            </button>
-            <p className='text-center justify-center text-[12px] justify-center gap-[3px] flex items-center'>
-                Low resolution<ToolBox text={"Low resolution and a bit tedious - but it's free"} />
-            </p>
+  return (
+    <div className="flex items-center gap-[10px] flex-col justify-center mt-[10px] w-full">
+      <p className="text-[18px] text-center leading-[1] max-w-[380px] lg:max-w-[540px] mb-4">
+        {renderInfo.title}
+      </p>
+      {loading ? (
+        <div
+          className="overlay"
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backdropFilter: "blur(8px)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 1000,
+          }}
+        >
+          <div className="centered-spinner" style={{ zIndex: 1001 }}>
+            <TailSpin
+              visible={loading}
+              width={50}
+              height={50}
+              color={siteConfig.buttonColor}
+            />
+          </div>
         </div>
-        <div className='w-full flex flex-col gap-[5px]'>
-            <a title="This is a tooltip message"
-                href={promotedURL}
-                className="
-                justify-center
-                inline-flex
-                rounded-lg
-                py-3
-                px-5
-                bg-white
-                text-[black]
-                border-2
-                border-black
-                font-medium
-                ease-in
-                duration-300
-                hover:bg-gray-100
-                dark:shadow-button
-                dark:hover:button-gradient-hover
-                transform 
-                hover:scale-110
-                flex
-                items-center
-                justify-center
-                transition 
-                duration-500 
-                ease-in-out"
-            >
-                Download HD
-            </a>
-            <p className='text-center justify-center text-[12px] justify-center gap-[3px] flex items-center'>
-                From {siteConfig.promotedBrand} for free<ToolBox text={"Download in HD from literally any website"} />
-            </p>
+      ) : (
+        ""
+      )}
+      <div className="flex flex-col lg:flex-row gap-[0px] lg:gap-[10px] items-center lg:items-center rounded-[10px] px-[10px]">
+        <div className="px-[10px] sm:px-0 mr-[20px] lg:mr-[30px]">
+          <Image
+            style={{ width: "auto", height: "auto" }}
+            className="max-h-[220px] lg:max-h-[190px] rounded-[10px]"
+            src={renderInfo.thumbnail}
+            alt=""
+            width={1200}
+            height={600}
+          />
         </div>
-    </div>
-</div>
+        <div className="flex flex-col gap-[20px] justify-center lg:px-0 px-[40px] sm:px-0 sm:max-w-[300px] h-full lg:max-w-[220px] py-[15px] items-center">
+          <div className="flex flex-col gap-[5px] lg:gap-[20px] w-full h-full">
+         <div>
 
+          <p className="text-start justify-start text-[12px] justify-center gap-[3px] flex items-center">
+                Select resolution
+                <ToolBox
+                  text={"Select the resolution that you need"}
+                  />
+              </p>
+          <select
+            id="resolutionDropdown"
+            className="rounded-lg py-2 px-3 text-[12px] border-2 border-gray-300"
+            value={selectedResolutionIndex}
+            onChange={(e) => handleResolutionChange(e.target.value)}
+            style={{
+              padding: '10px',
+              fontSize: '14px',
+              appearance:'auto',
+              border: '2px solid #ddd',
+              borderRadius: '8px',
+              backgroundColor: '#fff',
+              color: '#333',
+              width: '200px',
+            }}
+            >
+            {filteredFormats.map((resolution, index) => (
+              <option
+              key={index}
+              value={index}
+              style={{
+                padding: '8px',
+                fontSize: '18px',
+                lineHeight:'10px',
+                backgroundColor: '#fff',
+                color: '#333',
+              }}
+              >
+                {resolution.resolution}
+              </option>
+            ))}
+          </select>
             </div>
+            <div className="w-full flex flex-col gap-[5px]">
+              <button
+                onClick={() =>
+                  handleDownloadButtonClick(
+                    filteredFormats[selectedResolutionIndex]?.url,
+                    renderInfo.title
+                  )
+                }
+                style={
+                  {
+                    "--main-color": siteConfig.buttonColor,
+                    opacity: isResolutionSelected ? 1 : 0.5,
+                  } as any
+                }
+                className="button-styling justify-center inline-flex rounded-lg py-3 px-5 text-white font-medium ease-in duration-300 hover:opacity-80"
+                disabled={!isResolutionSelected}
+              >
+                Download
+              </button>
+            </div>
+            <div className='w-full flex flex-col gap-[5px]'>
+            <a
+            target="_blank"
+            className={`${
+              isResolutionSelected ? 'enabled-link' : 'disabled-link'
+            } justify-center inline-flex rounded-lg py-3 px-5 bg-white text-[black] border-2 border-black font-medium ease-in duration-300 hover:bg-gray-100 dark:shadow-button dark:hover:button-gradient-hover transform hover:scale-110 flex items-center justify-center transition duration-500 ease-in-out`}
+            href={filteredFormats[selectedResolutionIndex]?.url}
+          >
+            Play/ Download Video
+          </a>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Render
-
+export default Render;
 
 const ToolBox = ({ text }) => {
-    return (
-        <span className="tooltip bg-[gray] w-[12px] h-[12px] flex items-center justify-center text-[12px] rounded-full text-[white]">
-            <span className="tooltiptext">{text}</span>
-            ?
-        </span>
-    )
-}
+  return (
+    <span className="tooltip bg-[gray] w-[12px] h-[12px] flex items-center justify-center text-[12px] rounded-full text-[white]">
+      <span className="tooltiptext">{text}</span>?
+    </span>
+  );
+};
