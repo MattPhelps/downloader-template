10bdb1d926f72ec148640126f9527ab9dfaa78ae
Matt Phelps <matt@leandme.com>
2024-01-08 10:46:36 +0100

    gogogo

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,7 +1,6 @@
 import CallToAction from "@/components/CallToAction";
 import Head from 'next/head';
 import Features from "@/components/Home/Features";
-import FeaturesList from "@/components/Home/FeaturesList";
 import Hero from "@/components/Home/Hero";
 import { Metadata } from "next";
 import siteConfig from '../../../siteConfig';
