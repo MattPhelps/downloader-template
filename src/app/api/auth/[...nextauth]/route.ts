80b680eff99ab74fd9dd429953b17ba563d67965
Matt Phelps <matt@leandme.com>
2024-03-04 22:30:01 +0100

    ok

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -31,7 +31,7 @@ export const authOptions: NextAuthOptions = {
           throw new Error("Please enter an email or password");
         }
 
-        // check to see if user already exis
+        // check to see if user already exisg
         const user = await prisma.user.findUnique({
           where: {
             email: credentials.email,
